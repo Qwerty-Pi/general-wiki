92bd91481c86c45e93c44f5895ea2089eae9d595
Qwerty-Pi <qwertypi@LAPTOP-SLB41II5.>
2025-10-03 10:33:34 +0800

    Change Format for HTML Page

--- ICPC/layout.php
+++ ICPC/layout.php
@@ -9,8 +9,33 @@
 	</script>
     <link rel="stylesheet" type="text/css" href="../index.css">
 	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Ubuntu:regular,bold&subset=Latin">
+
+	<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
 </head>
 <body>
-	<?php echo $content; ?>
+	<?php echo file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/ICPC/' . $_GET['file'] . '.html'); ?>
 </body>
+
+<script>
+	function loadFile(code) {
+		$.ajax ({
+			method: "GET",
+			url: window.location.origin + '/' + code.data("include"),
+			success: function(content) {
+				content = content
+					.replaceAll("<", "&lt;")
+					.replaceAll(">", "&gt;")
+				code.html(content)
+			}
+		})
+	};
+
+	$(function () {
+		var includes = $('[data-include]')
+		$.each(includes, function () {
+			loadFile($(this))
+		})
+	})
+</script>
+
 </html>

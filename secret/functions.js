f1ed3a4de382d664151ecab1c78c517bb2c5f2cc
Qwerty-Pi <qwertypi@LAPTOP-SLB41II5.>
2026-06-22 12:38:32 +0800

    add answers

--- secret/functions.js
+++ secret/functions.js
@@ -1,5 +1,5 @@
 
-async function appendQuestion(container, label, identifier, data) {
+async function appendQuestion(container, label, identifier, data, answer = null) {
     questionOrder = $("<div class='question-order'>").html(label)
     questionLabel = $("<div class='question-label'>").append(questionOrder)
     
@@ -15,23 +15,27 @@ async function appendQuestion(container, label, identifier, data) {
     options = $("<div class='option-container'>")
     const labels = "ABCDE"
     option_id = 0
-    for (const option of data.options) {
-        options
-        .append($("<div class='option'>")
-        .append(`<span class='option-label'>${labels[option_id]}. </span>`)
-        .append(option))
+    for (const option_content of data.options) {
+        label = labels[option_id]
+        option = ($("<div class='option'>")
+        .append(`<span class='option-label'>${label}. </span>`)
+        .append(option_content))
+        if (label == answer) {
+            option.addClass("answer-hidden")
+        }
+        options.append(option)
         option_id += 1
     }
 
     content = $("<div class='question-content'>")
     if (identifier) {
-        content.data('identifier', identifier[0] + ' ' + identifier[1])
+        content.data('identifier', identifier[0] + ' Q' + identifier[1])
     }
-    container.append(questionLabel)
     content.append(statement)
     content.append(options)
-
-    container.append(content);
+    
+    container.append(questionLabel)
+    container.append(content)
 }
 
 
@@ -47,6 +51,7 @@ async function fetchJson(paper, year, file) {
 
 questions = []
 topics = []
+answers = []
 
 async function loadQuestionJson(paper, year) {
     questions[year] = await fetchJson('2', year, 'questions')
@@ -56,12 +61,27 @@ async function loadTopicJson(paper, year) {
     topics[year] = await fetchJson('2', year, 'topics')
 }
 
+async function loadAnswerJson(paper, year) {
+    answers[year] = await fetchJson('2', year, 'answers')
+}
+
 var years = []
 for (year = 1980; year <= 2011; year++) {
     years.push(year)
 }
-years.push("2012-PP")
 years.push("2012-SP")
+years.push("2012-PP")
 for (year = 2012; year <= 2025; year++) {
     years.push(year)
+}
+
+function toggleAnswer(label) {
+    const content = label.next()
+    if (label.hasClass("answer-shown")) {
+        label.removeClass("answer-shown")
+        content.find(".answer").removeClass("answer").addClass("answer-hidden")
+    } else {
+        label.addClass("answer-shown")
+        content.find(".answer-hidden").removeClass("answer-hidden").addClass("answer")
+    }
 }
